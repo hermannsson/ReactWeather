75ef63860e888986ac5162910195c0edcd43d274
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2017-03-27 22:20:24 +0000

    Changed units to metric because

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=3f1ce8ce65f9cbc4ce6f949ac76f2b83';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=3f1ce8ce65f9cbc4ce6f949ac76f2b83&units=metric';
 
 module.exports = {
   getTemp: function (location) {
