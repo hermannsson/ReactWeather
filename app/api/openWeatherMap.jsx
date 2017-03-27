4b03b30c48a4e41dcc6c90ff3d01360b940baff8
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2017-03-27 20:43:50 +0000

    Add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -9,9 +9,9 @@ module.exports = {
 
     return axios.get(requestUrl).then(function(res) {
       if(res.data.cod && res.data.message) {
-        throw new Error(res.data.message);
-      } else {
         return res.data.main.temp;
+      } else {
+        throw new Error('Unable to fetch weather for that location');
       }
     }, function(res) {
       throw new Error(res.data.message);
