57ca4d03da0c07893d098a82919e84dfe9989814
Ingvi Hermanns <ingvi.hermannsson@gmail.com>
2017-03-28 10:16:00 +0000

    Add nav search support

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,11 @@
 var React = require('react');
-var { Link } = require('react-router');
+var {Link} = require('react-router');
 
-var Examples = props => {
+var Examples = (props) => {
   return (
     <div>
       <h1 className="text-center page-title">Examples</h1>
-      <p>Here are a few example locations to try out</p>
+      <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
           <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
@@ -15,7 +15,7 @@ var Examples = props => {
         </li>
       </ol>
     </div>
-  );
+  )
 };
 
 module.exports = Examples;
